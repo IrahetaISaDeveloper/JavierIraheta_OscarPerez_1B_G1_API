c2b227ad1bd206c1e1ab5a94ca5b3c1bd68b83c3
IrahetaISaDeveloper <20230669@ricaldone.edu.sv>
2025-10-07 09:31:45 -0600

    estructura

---
+++ src/main/java/JavierIraheta_OscarPerez_B_G1/FINALBOSS/Models/DTO/PeliculaDTO.java
@@ -0,0 +1,4 @@
+package JavierIraheta_OscarPerez_B_G1.FINALBOSS.Models.DTO;
+
+public class PeliculaDTO {
+}
---
+++ src/main/java/JavierIraheta_OscarPerez_B_G1/FINALBOSS/Repositories/RepositoryPelicula.java
@@ -0,0 +1,4 @@
+package JavierIraheta_OscarPerez_B_G1.FINALBOSS.Repositories;
+
+public interface RepositoryPelicula {
+}
