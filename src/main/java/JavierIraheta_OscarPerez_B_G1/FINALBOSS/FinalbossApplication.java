a9d0afcfe78dc3105e51839dee8f6c682cd8849f
IrahetaISaDeveloper <20230669@ricaldone.edu.sv>
2025-10-07 09:15:26 -0600

    Repo Subido

---
+++ src/main/java/JavierIraheta_OscarPerez_B_G1/FINALBOSS/FinalbossApplication.java
@@ -0,0 +1,13 @@
+package JavierIraheta_OscarPerez_B_G1.FINALBOSS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalbossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalbossApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JavierIraheta_OscarPerez_B_G1/FINALBOSS/FinalbossApplicationTests.java
@@ -0,0 +1,13 @@
+package JavierIraheta_OscarPerez_B_G1.FINALBOSS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalbossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
